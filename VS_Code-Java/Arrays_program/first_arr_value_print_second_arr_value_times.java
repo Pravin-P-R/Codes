a487402a1c72ba178a6128decce369cb7244c2da
Pravin-P-R <pravinmcp004@gmail.com>
2025-11-27 10:30:53 +0530

    mini update

--- VS_Code-Java/Arrays_program/first_arr_value_print_second_arr_value_times.java
+++ VS_Code-Java/Arrays_program/first_arr_value_print_second_arr_value_times.java
@@ -7,7 +7,7 @@ public class first_arr_value_print_second_arr_value_times {
         Scanner ip = new Scanner(System.in);
         System.out.print("Enter Array Size : ");
         int size = ip.nextInt();
-        
+
         int[] matrix1 = new int[size];
         int[] matrix2 = new int[size];
         int index1 = 0, index2 = 0;
@@ -24,7 +24,8 @@ public class first_arr_value_print_second_arr_value_times {
         }
         process(matrix1, matrix2);
     }
-     public static void process(int[] data1,int[] data2) {
+
+    public static void process(int[] data1, int[] data2) {
         for (int row_index = 0; row_index < data1.length; row_index++) {
             for (int column_imdex = 0; column_imdex < data2[row_index]; column_imdex++) {
                 System.out.print(data1[row_index]);
